194d8c51d07fbdbb08cf3366b9172a826acdf5a5
Danish Kumar <70554986+DHANISH-25@users.noreply.github.com>
2026-09-10 11:24:18 +0530

    Delete java Practical 08-09-26 directory

--- java Practical 08-09-26/Practical 2/DisplayCollegeName.java
+++
@@ -1,6 +0,0 @@
-public class DisplayCollegeName {
-    public static void main(String[] args) {
-        String collegeName = "SEEMANTA ENGINEERING COLLEGE";
-        System.out.println("College Name: " + collegeName);
-    }
-}
